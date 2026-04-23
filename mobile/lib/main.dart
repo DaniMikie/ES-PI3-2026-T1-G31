1ecd98ca5ca081d8844a86075f4a00e32f7ed189
Dani Mikie <dgoncalvessp@gmail.com>
2026-04-22 23:26:57 -0300

    feat: tela de cadastro e integração das telas com o backend

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,17 @@
+/**
+ * Conexão do flutter com firebase
+ * Autor: Daniela Mikie Kikuchi Gonçalves | RA: 25003068
+ */
+
+
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
